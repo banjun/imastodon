a55aeebfa33cb86d3fc60c4f14540dd61c78ccfb
banjun <banjun@gmail.com>
2017-06-05 00:07:31 +0900

    support stream in home timeline

--- imastodon/HomeViewController.swift
+++ imastodon/HomeViewController.swift
@@ -2,9 +2,11 @@ import Foundation
 import Eureka
 import SVProgressHUD
 import MastodonKit
+import UserNotifications
 
 class HomeViewController: TimelineViewController {
     let instanceAccount: InstanceAccout
+    private var userStream: Stream?
 
     init(instanceAccount: InstanceAccout) {
         self.instanceAccount = instanceAccount
@@ -13,6 +15,10 @@ class HomeViewController: TimelineViewController {
         toolbarItems = [UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(showPost))]
     }
     required init?(coder aDecoder: NSCoder) {fatalError()}
+
+    deinit {
+        userStream?.close()
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -24,6 +30,37 @@ class HomeViewController: TimelineViewController {
 
         if statuses.isEmpty {
             fetch()
+        }
+        if userStream == nil {
+            reconnectStream()
+        }
+    }
+
+    private func reconnectStream() {
+        userStream?.close()
+
+        userStream = Stream(userTimelineForHost: instanceAccount.instance.uri, token: instanceAccount.accessToken)
+        userStream?.updateSignal.observeResult { [weak self] r in
+            DispatchQueue.main.async {
+                switch r {
+                case let .success(s): self?.append([s])
+                case let .failure(e): self?.append([e.errorStatus])
+                }
+            }
+        }
+        userStream?.notificationSignal.observeResult { [weak self] r in
+            DispatchQueue.main.async {
+                switch r {
+                case let .success(n):
+                    // NSLog("%@", "notification: \(n.account), \(n.type), \(String(describing: n.status?.textContent))")
+                    let content = UNMutableNotificationContent()
+                    content.title = "\(n.account.displayName) \(n.type)"
+                    content.body = n.status?.textContent ?? "you"
+                    UNUserNotificationCenter.current()
+                        .add(UNNotificationRequest(identifier: "notification \(n.id)", content: content, trigger: nil))
+                case let .failure(e): self?.append([e.errorStatus])
+                }
+            }
         }
     }
 
