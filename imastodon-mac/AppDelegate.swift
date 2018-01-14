7cf283ccf89a72bf5cbbf838e8f9a9a9b5ebe354
banjun <banjun@gmail.com>
2018-01-14 21:11:04 +0900

    add mac app target

---
+++ imastodon-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  imastodon-mac
+//
+//  Created by BAN Jun on 2018/01/14.
+//  Copyright © 2018 banjun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ imastodon-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  imastodon-mac
+//
+//  Created by BAN Jun on 2018/01/14.
+//  Copyright © 2018 banjun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
