e0d9b0152104009958d5373ae4bd5663e693097b
banjun <banjun@gmail.com>
2018-01-16 00:58:52 +0900

    autolayout status heights without using usesAutomaticRowHeights that does not work with code-less

--- imastodon-mac/ViewController.swift
+++ imastodon-mac/ViewController.swift
@@ -10,6 +10,10 @@ private let baseURL = "https://imastodon.net"
 private let token = ""
 
 class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    private lazy var scrollView = NSScrollView() ※ { sv in
+        sv.hasVerticalScroller = true
+        sv.documentView = timelineView
+    }
     private let timelineView = NSTableView(frame: .zero)
     private lazy var timelineDiff: TableViewDiffCalculator<ID> = .init(tableView: self.timelineView)
     private var timeline: [Status] = [] {didSet {applyDwifft()}}
@@ -20,22 +24,19 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.translatesAutoresizingMaskIntoConstraints = false
-
         timelineView.dataSource = self
         timelineView.delegate = self
-        timelineView.usesAutomaticRowHeights = true
+//        timelineView.usesAutomaticRowHeights = true
         let tc = NSTableColumn() ※ {
             $0.identifier = NSUserInterfaceItemIdentifier(rawValue: "Status")
-            $0.maxWidth = 1024
         }
         timelineView.addTableColumn(tc)
         timelineView.register(NSNib(nibNamed: NSNib.Name(rawValue: "StatusCellView"), bundle: nil), forIdentifier: tc.identifier)
         applyDwifft()
 
-        let autolayout = view.northLayoutFormat([:], ["timeline": timelineView])
-        autolayout("H:|[timeline(>=128)]|")
-        autolayout("V:|[timeline(>=512,<=1024)]|")
+        let autolayout = view.northLayoutFormat([:], ["sv": scrollView])
+        autolayout("H:|[sv(>=128)]|")
+        autolayout("V:|[sv(>=128)]|")
 
         var req = URLRequest(url: URL(string: baseURL + "/api/v1/streaming/public/local")!)
         req.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
@@ -55,9 +56,18 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         }
     }
 
+    override func viewDidAppear() {
+        super.viewDidAppear()
+
+        // fake data
+        let a = Account(id: "0", username: "banjun", acct: "banjun", display_name: "ばんじゅん🍓", locked: false, created_at: "2018-01-15T13:27:36Z", followers_count: 42, following_count: 42, statuses_count: 42, note: "note", url: "url", avatar: "https://cdn.imastodon.net/accounts/avatars/000/000/026/original/869612d5adbf136f.png", avatar_static: "avatar_static", header: "header", header_static: "header_static")
+        let ss = (0..<10).map {Status(id: ID(stringLiteral: "\($0)"), uri: "uri", url: "url", account: a, in_reply_to_id: nil, in_reply_to_account_id: nil, reblog: nil, content: "このステージにいると…お腹が空きますね…いえ、なんでもありません", created_at: "2018-01-15T13:27:36Z", reblogs_count: 0, favourites_count: 0, reblogged: nil, favourited: false, sensitive: false, spoiler_text: "", visibility: "public", media_attachments: [], mentions: [], tags: [], application: nil, language: nil, pinned: false)}
+        timeline.append(contentsOf: ss)
+    }
+
     override func viewDidLayout() {
         super.viewDidLayout()
-        timelineView.tableColumns.first?.width = timelineView.frame.width
+        timelineView.noteHeightOfRows(withIndexesChanged: NSIndexSet(indexesIn: timelineView.rows(in: scrollView.contentView.bounds)) as IndexSet)
     }
 
     func numberOfRows(in tableView: NSTableView) -> Int {
@@ -68,40 +78,49 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         guard let tableColumn = tableColumn else { return nil }
         let s = timeline[row]
         let cellView = tableView.makeView(withIdentifier: tableColumn.identifier, owner: self) as! StatusTableCellView
-        cellView.bodyLabel.stringValue = s.textContent
-        cellView.nameLabel.stringValue = s.account.displayNameOrUserName
-        if let avatarURL = s.account.avatarURL(baseURL: URL(string: baseURL)!) {
-            cellView.iconView.kf.setImage(with: avatarURL)
-        }
+        cellView.setStatus(s)
         return cellView
     }
 
-//    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-//        let s = timeline[row]
-//        let cellView = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Status"), owner: self) as! StatusTableCellView
-//        cellView.bodyLabel.stringValue = s.textContent
-//        cellView.layoutSubtreeIfNeeded()
-//        return cellView.frame.height
-//    }
+    private lazy var layoutCell = StatusTableCellView() ※ {$0.awakeFromNib()}
+
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        layoutCell.setStatus(timeline[row], widthConstraintConstant: scrollView.contentView.bounds.width)
+        return layoutCell.fittingSize.height
+    }
+}
+
+final class AutoLayoutLabel: NSTextField {
+    init() {
+        super.init(frame: .zero)
+        setupAutolayoutable()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupAutolayoutable()
+    }
+
+    private func setupAutolayoutable() {
+        // NOTE: preferredMaxLayoutWidth should be set as desired
+        isEditable = false // makes multiline intrinsic content size work
+        maximumNumberOfLines = 0 // multiline
+    }
 }
 
 @objc class StatusTableCellView: NSTableCellView {
     let iconView = NSImageView()
-    let nameLabel = NSTextField() ※ { l in
-        l.maximumNumberOfLines = 1
+    let nameLabel = AutoLayoutLabel() ※ { l in
         l.textColor = .gray
         l.isBezeled = false
     }
-    let bodyLabel = NSTextField() ※ { l in
-        l.maximumNumberOfLines = 0
+    let bodyLabel = AutoLayoutLabel() ※ { l in
         l.textColor = .black
         l.isBezeled = false
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
-        bodyLabel.textColor = .black
 
         let autolayout = northLayoutFormat([:], [
             "icon": iconView,
@@ -111,5 +130,15 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
         autolayout("H:|[body]|")
         autolayout("V:|[icon(==32)][body]|")
         autolayout("V:|[name][body]|")
+    }
+
+    func setStatus(_ status: Status, widthConstraintConstant: CGFloat? = nil) {
+        _ = widthConstraintConstant.map {bodyLabel.preferredMaxLayoutWidth = $0}
+
+        bodyLabel.stringValue = status.textContent
+        nameLabel.stringValue = status.account.displayNameOrUserName
+        if let avatarURL = status.account.avatarURL(baseURL: URL(string: baseURL)!) {
+            iconView.kf.setImage(with: avatarURL)
+        }
     }
 }
