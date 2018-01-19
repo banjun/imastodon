e467710a6d074bc3a881bcc93991e9ed651a5a78
banjun <banjun@gmail.com>
2018-01-19 21:38:30 +0900

    process filters in background

--- imastodon-mac/LocalTLWindowController.swift
+++ imastodon-mac/LocalTLWindowController.swift
@@ -30,13 +30,15 @@ final class TimelineViewModel {
             options: [.caseInsensitive, .diacriticInsensitive, .widthInsensitive],
             locale: .current) != nil}
         }}
+    private let backgroundQueue = QueueScheduler(name: "TimelineViewModel.background")
     private(set) lazy var filteredTimeline = Property<[Status]>(initial: timeline.value, then:
         filterPredicate.producer
-            .debounce(0.1, on: QueueScheduler.main)
+            .debounce(0.1, on: backgroundQueue)
             .combineLatest(with: timeline)
-            .throttle(0.1, on: QueueScheduler.main)
+            .throttle(0.1, on: backgroundQueue)
             // TODO: consider scan to apply filter only on inserted toots
-            .map {filterPredicate, timeline in filterPredicate.map {timeline.filter($0)} ?? timeline})
+            .map {filterPredicate, timeline in filterPredicate.map {timeline.filter($0)} ?? timeline}
+            .observe(on: UIScheduler()))
 }
 
 final class LocalTLViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
