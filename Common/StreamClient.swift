0fd223fa13b626ec037469125a1e3cd3e272664e
banjun <banjun@gmail.com>
2018-01-28 12:40:46 +0900

    migrate SignalHolder to SignalRetainingDisposable omitting management of lifetimes by making use of capture by Disposable

--- Common/StreamClient.swift
+++ Common/StreamClient.swift
@@ -9,12 +9,12 @@ final class StreamClient {
     private let instanceAccount: InstanceAccout
     private var activeSignals: ActiveSignals = .init()
     private struct ActiveSignals {
-        var local: SignalHolder<SSEvent, NoError>?
-        var localToots: SignalHolder<Status, NoError>?
-        var user: SignalHolder<SSEvent, NoError>?
-        var homeToots: SignalHolder<Status, NoError>?
-        var notifications: SignalHolder<API.Notification, NoError>?
-        var hashtags: [String: SignalHolder<Status, NoError>] = [:]
+        weak var local: Signal<SSEvent, NoError>?
+        weak var localToots: Signal<Status, NoError>?
+        weak var user: Signal<SSEvent, NoError>?
+        weak var homeToots: Signal<Status, NoError>?
+        weak var notifications: Signal<API.Notification, NoError>?
+        var hashtags: [String: Signal<Status, NoError>] = [:]
     }
 
     init(instanceAccount: InstanceAccout) {
@@ -40,21 +40,21 @@ final class StreamClient {
 
     // local events signal sharing underlying network connection
     func local(during lifetime: Lifetime) -> Signal<SSEvent, NoError> {
-        defer {activeSignals.local?.lifetimes.append(lifetime)}
+        defer {lifetime += SignalRetainingDisposable(activeSignals.local)}
         return (activeSignals.local?.signal ?? (
             signal(relativeURL: "/api/v1/streaming/public/local")
-                ※ {activeSignals.local = SignalHolder($0)}))
+                ※ {activeSignals.local = $0}))
     }
 
     // local toots signal sharing results of Codable decodes
     func localToots(during lifetime: Lifetime) -> Signal<Status, NoError> {
-        defer {activeSignals.localToots?.lifetimes.append(lifetime)}
+        defer {lifetime += SignalRetainingDisposable(activeSignals.localToots)}
         return (activeSignals.localToots?.signal ?? (
             local(during: lifetime)
                 .filter {$0.type == "update"}
                 .filterMap {$0.data.data(using: .utf8)}
                 .filterMap {try? JSONDecoder().decode(Status.self, from: $0)}
-                ※ {activeSignals.localToots = SignalHolder($0)}))
+                ※ {activeSignals.localToots = $0}))
     }
 
     // local deleted ids signal that is not sharing results of Codable decodes, sharing underlying networks
@@ -66,21 +66,21 @@ final class StreamClient {
 
     // user events signal sharing underlying network connection
     func user(during lifetime: Lifetime) -> Signal<SSEvent, NoError> {
-        defer {activeSignals.user?.lifetimes.append(lifetime)}
+        defer {lifetime += SignalRetainingDisposable(activeSignals.user)}
         return (activeSignals.user?.signal ?? (
             signal(relativeURL: "/api/v1/streaming/user")
-                ※ {activeSignals.user = SignalHolder($0)}))
+                ※ {activeSignals.user = $0}))
     }
 
     // home toots signal sharing results of Codable decodes
     func homeToots(during lifetime: Lifetime) -> Signal<Status, NoError> {
-        defer {activeSignals.homeToots?.lifetimes.append(lifetime)}
+        defer {lifetime += SignalRetainingDisposable(activeSignals.homeToots)}
         return (activeSignals.homeToots?.signal ?? (
             user(during: lifetime)
                 .filter {$0.type == "update"}
                 .filterMap {$0.data.data(using: .utf8)}
                 .filterMap {try? JSONDecoder().decode(Status.self, from: $0)}
-                ※ {activeSignals.homeToots = SignalHolder($0)}))
+                ※ {activeSignals.homeToots = $0}))
     }
 
     // home deleted ids signal that is not sharing results of Codable decodes, sharing underlying networks
@@ -92,13 +92,13 @@ final class StreamClient {
 
     // user notifications signal sharing results of Codable decodes
     func notifications(during lifetime: Lifetime) -> Signal<API.Notification, NoError> {
-        defer {activeSignals.notifications?.lifetimes.append(lifetime)}
+        defer {lifetime += SignalRetainingDisposable(activeSignals.notifications)}
         return (activeSignals.notifications?.signal ?? (
             user(during: lifetime)
                 .filter {$0.type == "notification"}
                 .filterMap {$0.data.data(using: .utf8)}
                 .filterMap {try? JSONDecoder().decode(Notification.self, from: $0)}
-                ※ {activeSignals.notifications = SignalHolder($0)}))
+                ※ {activeSignals.notifications = $0}))
     }
 }
 
@@ -112,19 +112,14 @@ private extension SignalProducer {
     }
 }
 
-final class SignalHolder<Value, Error: Swift.Error> {
-    private (set) var signal: Signal<Value, Error>?
-    var lifetimes: [Lifetime] = [] {
-        didSet {
-            cancelObserveCompleted?.dispose()
-            cancelObserveCompleted = SignalProducer(lifetimes.map {$0.ended}).flatten(.concat).startWithCompleted { [unowned self] in
-                self.signal = nil
-            }
-        }
-    }
-    var cancelObserveCompleted: Disposable?
-
-    init(_ signal: Signal<Value, Error>) {
+/// retain strong ref to the signal and release it on dispose
+/// can be used for appending to the underlying lifetime by `lifetime += SignalRetainingDisposable(signal to be retained)`
+final class SignalRetainingDisposable<Value, Error: Swift.Error>: Disposable {
+    private var signal: Signal<Value, Error>?
+    private lazy var base: AnyDisposable = .init {self.signal = nil}
+    init(_ signal: Signal<Value, Error>?) {
         self.signal = signal
     }
+    var isDisposed: Bool {return base.isDisposed}
+    func dispose() {base.dispose()}
 }
