1ca922dfc95497ef2932785081052afd0d042a78
banjun <banjun@gmail.com>
2020-02-09 01:36:44 +0900

    enable multiple windows with scene delegate for iOS 13

--- imastodon/AppDelegate.swift
+++ imastodon/AppDelegate.swift
@@ -10,16 +10,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupNotifications()
-        window = UIWindow() ※ { w in
-            let vc = ViewController()
-            w.rootViewController = UINavigationController(rootViewController: vc)
-            w.makeKeyAndVisible()
-        }
         SVProgressHUD.setDefaultMaskType(.black)
 
         ImageCache.default.memoryStorage.config.totalCostLimit = 100 * 120 * 120 // avatar * 100
 
+        guard #available(iOS 13, *) else {
+            window = UIWindow() ※ { w in
+                let vc = ViewController()
+                w.rootViewController = UINavigationController(rootViewController: vc)
+                w.makeKeyAndVisible()
+            }
+            return true
+        }
         return true
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        UISceneConfiguration(name: nil, sessionRole: .windowApplication) ※ {$0.delegateClass = SceneDelegate.self}
     }
 
     private func setupNotifications() {
